e6a18ea34641166b045053c2a28ccb22c317b76d
KRAKEN504C <32463878+KRAKEN504C@users.noreply.github.com>
2017-12-13 01:10:25 +0100

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -12,6 +12,8 @@ int przesu = 0;
 float czas1=0;
 float czas2 = 0;
 
+int do_wykresu = 0;
+
 /*
 Rand_MAX = 32767;
 3 x Rand_MAX = 98301;
@@ -111,7 +113,7 @@ int partition(int tablica[], int p, int r) // dzielimy tablice na dwie czesci, w
 		while (tablica[i] < x) // dopoki elementy sa mniejsze od x
 			i++;
 
-		przesu++;
+		przesu++; //<-------------------- PRZESUNIECIE++!!! -------------------->
 
 		if (i < j) // zamieniamy miejscami gdy i < j
 		{
@@ -120,7 +122,7 @@ int partition(int tablica[], int p, int r) // dzielimy tablice na dwie czesci, w
 			tablica[j] = w;
 			i++;
 			j--;
-			przestaw++;
+			przestaw++; //<-------------------- PRZESTAWIENIE++!!! -------------------->
 		}
 		else // gdy i >= j zwracamy j jako punkt podzialu tablicy
 			return j;
@@ -170,7 +172,7 @@ int shellSort(int arr[], int n)
 
 			//  put temp (the original a[i]) in its correct location
 			arr[j] = temp;
-			przestaw++;
+			przestaw++; //<-------------------- PRZESTAWIENIE++!!! -------------------->
 		}
 	}
 
@@ -192,7 +194,7 @@ void merge(int pocz, int sr, int kon,int tab[],int t[])
 		else
 			tab[q++] = t[j++];
 
-		przestaw++;
+		przestaw++; //<-------------------- PRZESTAWIENIE++!!! -------------------->
 	}
 	while (i <= sr) tab[q++] = t[i++]; // Przeniesienie nie skopiowanych danych ze zbioru pierwszego w przypadku, gdy drugi zbiór siê skoñczy³
 }
@@ -216,6 +218,15 @@ void mergesort(int pocz, int kon, int tab[], int t[])
 void wszystko(int n)
 {
 	string file;
+	string wyniczek;
+
+	wyniczek = "wynik";
+	wyniczek += to_string(n);
+	wyniczek += ".txt";
+
+	ofstream wynik(wyniczek);
+
+	//wynik.open(wyniczek, ios::in | ios::out);
 
 	int *tab = new int[n];
 	int *ortab = new int[n];
@@ -236,8 +247,6 @@ void wszystko(int n)
 	file += ".txt";
 	cout << file << endl;
 	saveToFile2(tab, n, file);
-	
-	//int *t = new int[n];
 	czas1 = clock();
 	QuickSort(tab, 0, n - 1);
 	czas2 = clock();
@@ -248,6 +257,18 @@ void wszystko(int n)
 	file += ".txt";
 	cout << file << endl;
 	saveToFile2(tab, n, file);
+
+	wynik << czas2;
+	wynik << endl;
+	/*
+	wynik << przestaw;
+	wynik << endl;
+	wynik << przesu;
+	wynik << endl;
+	*/
+	wynik << przestaw + przesu;
+	wynik << endl;
+
 	czas1 = 0;
 	czas2 = 0;
 	przestaw = 0;
@@ -267,6 +288,12 @@ void wszystko(int n)
 	file += ".txt";
 	cout << file << endl;
 	saveToFile2(tab, n, file);
+
+	wynik << czas2;
+	wynik << endl;
+	wynik << przestaw;
+	wynik << endl;
+
 	czas1 = 0;
 	czas2 = 0;
 	przestaw = 0;
@@ -286,6 +313,12 @@ void wszystko(int n)
 	file += ".txt";
 	cout << file << endl;
 	saveToFile2(tab, n, file);
+
+	wynik << czas2;
+	wynik << endl;
+	wynik << przestaw;
+	wynik << endl;
+
 	czas1 = 0;
 	czas2 = 0;
 	przestaw = 0;
@@ -295,11 +328,370 @@ void wszystko(int n)
 		tab[i] = ortab[i];
 }
 
+void ladowanie_wykresu(int tab1[], int tab2[],int n)
+{
+	fstream wynik;
+
+	string wyniczek;
+
+	wyniczek = "wynik";
+	wyniczek += to_string(n);
+	wyniczek += ".txt";
+
+	wynik.open(wyniczek, ios::in | ios::out);
+	/*
+	switch (n) 
+	{
+	case 10000:
+		wynik >> tab1[do_wykresu];
+		wynik >> tab2[do_wykresu];
+		do_wykresu++;
+		wynik >> tab1[do_wykresu];
+		wynik >> tab2[do_wykresu];
+		do_wykresu++;
+		wynik >> tab1[do_wykresu];
+		wynik >> tab2[do_wykresu];
+		do_wykresu++;
+		break;
+	case 25000:
+		break;
+	case 50000:
+		break;
+	case 75000:
+		break;
+	case 100000:
+		break;
+	case 150000:
+		break;
+	}
+	*/
+
+	/*
+	if (n == 10000)
+	{
+
+	}else if (n == 25000)
+	{
+
+	}else if (n == 50000)
+	{
+
+	}else if (n == 75000)
+	{
+
+	}
+	else if (n == 100000)
+	{
+
+	}
+	else if (n == 150000)
+	{
+
+	}
+	*/
+
+	wynik >> tab1[do_wykresu];
+	wynik >> tab2[do_wykresu];
+	do_wykresu++;
+	wynik >> tab1[do_wykresu];
+	wynik >> tab2[do_wykresu];
+	do_wykresu++;
+	wynik >> tab1[do_wykresu];
+	wynik >> tab2[do_wykresu];
+	do_wykresu++;
+
+}
+
 void wykres()
 {
 	//Wykres liczby przestawien od ilosci danych.
+	int wykres_przestaw[18];
 
 	//Wykres czasu wykonania funkcji sortujacych od ilosci danych
+	int wykres_czas[18];
+
+	ladowanie_wykresu(wykres_czas, wykres_przestaw, 10000);
+	ladowanie_wykresu(wykres_czas, wykres_przestaw, 25000);
+	ladowanie_wykresu(wykres_czas, wykres_przestaw, 50000);
+	ladowanie_wykresu(wykres_czas, wykres_przestaw, 75000);
+	ladowanie_wykresu(wykres_czas, wykres_przestaw, 100000);
+	ladowanie_wykresu(wykres_czas, wykres_przestaw, 150000);
+
+	for (int i = 0; i < 18; i++)
+	{
+		cout << wykres_czas[i] << endl;
+		cout << wykres_przestaw[i] << endl;
+	}
+
+	int pomoc_wykresowa = 1;
+	int en = 10000;
+
+	/*
+	QuickSort(wykres_czas, 0, 18 - 1);
+	QuickSort(wykres_przestaw, 0, 18 - 1);
+
+	for (int i = 0; i < 18; i++)
+	{
+		cout << wykres_czas[i] << endl;
+		cout << wykres_przestaw[i] << endl;
+	}
+	*/
+	system("Pause");
+	system("cls");
+	char wynik_czas[10][18] = {};
+	char wynik_przestaw[10][18] = {};
+
+	for (int i = 0; i < 18; i++)
+	{
+		if (wykres_czas[i] <= 5)
+		{
+			wynik_czas[0][i] = 254;
+		}else if (wykres_czas[i] >= 6 && wykres_czas[i] <= 10)
+		{
+			wynik_czas[0][i] = 254;
+			wynik_czas[1][i] = 254;
+		}else if (wykres_czas[i] >= 11 && wykres_czas[i] <= 15)
+		{
+			wynik_czas[0][i] = 254;
+			wynik_czas[1][i] = 254;
+			wynik_czas[2][i] = 254;
+		}else if (wykres_czas[i] >= 16 && wykres_czas[i] <= 20)
+		{
+			wynik_czas[0][i] = 254;
+			wynik_czas[1][i] = 254;
+			wynik_czas[2][i] = 254;
+			wynik_czas[3][i] = 254;
+		}else if (wykres_czas[i] >= 21 && wykres_czas[i] <= 25)
+		{
+			wynik_czas[0][i] = 254;
+			wynik_czas[1][i] = 254;
+			wynik_czas[2][i] = 254;
+			wynik_czas[3][i] = 254;
+			wynik_czas[4][i] = 254;
+		}else if (wykres_czas[i] >= 26 && wykres_czas[i] <= 30)
+		{
+			wynik_czas[0][i] = 254;
+			wynik_czas[1][i] = 254;
+			wynik_czas[2][i] = 254;
+			wynik_czas[3][i] = 254;
+			wynik_czas[4][i] = 254;
+			wynik_czas[5][i] = 254;
+		}else if (wykres_czas[i] >= 31 && wykres_czas[i] <= 35)
+		{
+			wynik_czas[0][i] = 254;
+			wynik_czas[1][i] = 254;
+			wynik_czas[2][i] = 254;
+			wynik_czas[3][i] = 254;
+			wynik_czas[4][i] = 254;
+			wynik_czas[5][i] = 254;
+			wynik_czas[6][i] = 254;
+		}else if (wykres_czas[i] >= 36 && wykres_czas[i] <= 40)
+		{
+			wynik_czas[0][i] = 254;
+			wynik_czas[1][i] = 254;
+			wynik_czas[2][i] = 254;
+			wynik_czas[3][i] = 254;
+			wynik_czas[4][i] = 254;
+			wynik_czas[5][i] = 254;
+			wynik_czas[6][i] = 254;
+			wynik_czas[7][i] = 254;
+		}else if (wykres_czas[i] >= 41 && wykres_czas[i] <= 45)
+		{
+
+			wynik_czas[0][i] = 254;
+			wynik_czas[1][i] = 254;
+			wynik_czas[2][i] = 254;
+			wynik_czas[3][i] = 254;
+			wynik_czas[4][i] = 254;
+			wynik_czas[5][i] = 254;
+			wynik_czas[6][i] = 254;
+			wynik_czas[7][i] = 254;
+			wynik_czas[8][i] = 254;
+		}else if (wykres_czas[i] >= 46)
+		{
+			wynik_czas[0][i] = 254;
+			wynik_czas[1][i] = 254;
+			wynik_czas[2][i] = 254;
+			wynik_czas[3][i] = 254;
+			wynik_czas[4][i] = 254;
+			wynik_czas[5][i] = 254;
+			wynik_czas[6][i] = 254;
+			wynik_czas[7][i] = 254;
+			wynik_czas[8][i] = 254;
+			wynik_czas[9][i] = 254;
+		}
+	}
+
+	cout << "\tWYKRES DLA CZASU WYKONYWANIE POSZCZEGOLNYCH FUNKCJI " << endl;
+	cout << "czas jest podany w przedziale od 0 do 46 sekund. jedna kropka oznacza 5 sekund" << endl;
+	cout << "pierwszy poziom to quickSort, drugi to shellSort, a trzeci to mergeSort." << endl;
+	cout << "dla " << en << endl;
+	for (int i = 0; i < 18; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			cout << wynik_czas[j][i];
+		}
+		//pomoc_wykresowa++;
+		switch (pomoc_wykresowa)
+		{
+		case 3:
+			en = 25000;
+			break;
+		case 6:
+			en = 50000;
+			break;
+		case 9:
+			en = 75000;
+			break;
+		case 12:
+			en = 100000;
+			break;
+		case 15:
+			en = 150000;
+			break;
+		}
+		if (pomoc_wykresowa == 18) { pomoc_wykresowa = 1; }
+		if (pomoc_wykresowa % 3 == 0)
+		{
+			cout << endl << "dla " << en;
+		}
+		pomoc_wykresowa++;
+		cout << endl;
+	}
+
+
+	pomoc_wykresowa = 1;
+	en = 10000;
+	cout << endl << endl;
+
+	for (int i = 0; i < 18; i++)
+	{
+		if (wykres_przestaw[i] <= 100000)
+		{
+			wynik_przestaw[0][i] = 254;
+		}
+		else if (wykres_przestaw[i] >= 100001 && wykres_przestaw[i] <= 200000)
+		{
+			wynik_przestaw[0][i] = 254;
+			wynik_przestaw[1][i] = 254;
+		}
+		else if (wykres_przestaw[i] >= 200001 && wykres_przestaw[i] <= 400000)
+		{
+			wynik_przestaw[0][i] = 254;
+			wynik_przestaw[1][i] = 254;
+			wynik_przestaw[2][i] = 254;
+		}
+		else if (wykres_przestaw[i] >= 400001 && wykres_przestaw[i] <= 600000)
+		{
+			wynik_przestaw[0][i] = 254;
+			wynik_przestaw[1][i] = 254;
+			wynik_przestaw[2][i] = 254;
+			wynik_przestaw[3][i] = 254;
+		}
+		else if (wykres_przestaw[i] >= 600001 && wykres_przestaw[i] <= 800000)
+		{
+			wynik_przestaw[0][i] = 254;
+			wynik_przestaw[1][i] = 254;
+			wynik_przestaw[2][i] = 254;
+			wynik_przestaw[3][i] = 254;
+			wynik_przestaw[4][i] = 254;
+		}
+		else if (wykres_przestaw[i] >= 800001 && wykres_przestaw[i] <= 1000000)
+		{
+			wynik_przestaw[0][i] = 254;
+			wynik_przestaw[1][i] = 254;
+			wynik_przestaw[2][i] = 254;
+			wynik_przestaw[3][i] = 254;
+			wynik_przestaw[4][i] = 254;
+			wynik_przestaw[5][i] = 254;
+		}
+		else if (wykres_przestaw[i] >= 1000001 && wykres_przestaw[i] <= 1200000)
+		{
+			wynik_przestaw[0][i] = 254;
+			wynik_przestaw[1][i] = 254;
+			wynik_przestaw[2][i] = 254;
+			wynik_przestaw[3][i] = 254;
+			wynik_przestaw[4][i] = 254;
+			wynik_przestaw[5][i] = 254;
+			wynik_przestaw[6][i] = 254;
+		}
+		else if (wykres_przestaw[i] >= 1200001 && wykres_przestaw[i] <= 1400000)
+		{
+			wynik_przestaw[0][i] = 254;
+			wynik_przestaw[1][i] = 254;
+			wynik_przestaw[2][i] = 254;
+			wynik_przestaw[3][i] = 254;
+			wynik_przestaw[4][i] = 254;
+			wynik_przestaw[5][i] = 254;
+			wynik_przestaw[6][i] = 254;
+			wynik_przestaw[7][i] = 254;
+		}
+		else if (wykres_przestaw[i] >= 1400001 && wykres_przestaw[i] <= 1600000)
+		{
+
+			wynik_przestaw[0][i] = 254;
+			wynik_przestaw[1][i] = 254;
+			wynik_przestaw[2][i] = 254;
+			wynik_przestaw[3][i] = 254;
+			wynik_przestaw[4][i] = 254;
+			wynik_przestaw[5][i] = 254;
+			wynik_przestaw[6][i] = 254;
+			wynik_przestaw[7][i] = 254;
+			wynik_przestaw[8][i] = 254;
+		}
+		else if (wykres_przestaw[i] >= 1600001)
+		{
+			wynik_przestaw[0][i] = 254;
+			wynik_przestaw[1][i] = 254;
+			wynik_przestaw[2][i] = 254;
+			wynik_przestaw[3][i] = 254;
+			wynik_przestaw[4][i] = 254;
+			wynik_przestaw[5][i] = 254;
+			wynik_przestaw[6][i] = 254;
+			wynik_przestaw[7][i] = 254;
+			wynik_przestaw[8][i] = 254;
+			wynik_przestaw[9][i] = 254;
+		}
+	}
+
+	cout << "\tWYKRES DLA ILOSCI WYKONANYCH OPERACJI DLA POSZCZEGOLNYCH FUNKCJI " << endl;
+	cout << "Iloscsc wykonanych operacji podana jest w  przedziale od 0 do 1.600.000. jenda kropka oznacza 200.000 operacji" << endl;
+	cout << "pierwszy poziom to quickSort, drugi to shellSort, a trzeci to mergeSort." << endl;
+	cout << "dla " << en << endl;
+	for (int i = 0; i < 18; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			cout << wynik_przestaw[j][i];
+		}
+		//pomoc_wykresowa++;
+		switch (pomoc_wykresowa)
+		{
+		case 3:
+			en = 25000;
+			break;
+		case 6:
+			en = 50000;
+			break;
+		case 9:
+			en = 75000;
+			break;
+		case 12:
+			en = 100000;
+			break;
+		case 15:
+			en = 150000;
+			break;
+		}
+		if (pomoc_wykresowa == 18) { pomoc_wykresowa = 1; }
+		if (pomoc_wykresowa % 3 == 0)
+		{
+			cout << endl << "dla " << en;
+		}
+		pomoc_wykresowa++;
+		cout << endl;
+	}
 
 }
 
@@ -440,7 +832,7 @@ int main()
 	wszystko(150000);
 
 
-
+	wykres();
 
 	delete[] tab; // zwolnienie tablicy zaalokowanej dynamicznie
 
